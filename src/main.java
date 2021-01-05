fa3c340c52f0780558c3c49cf4481a6c73dc12d5
xtreme6007 <prestonnichols96@gmail.com>
2021-01-05 12:00:40 -0600

    finished first method and finishing overloaded method

--- src/main.java
+++ src/main.java
@@ -1,11 +1,35 @@
 public class main {
     public static void main (String[] args) {
 
-
+getDurationString(193, 33);
 
     }
     public static String getDurationString(int minutes, int seconds){
+     if (minutes >= 0 && (seconds >= 0 && seconds <= 59)){
+         int hours = minutes / 60;
+         int newMin = minutes % 60;
+         System.out.println(hours + "h" + newMin + "m" + seconds + "s");
+         return (hours + "h" + newMin + "m" + seconds + "s");
 
+
+        }
+
+    return "Invalid Value";
+
+    }
+
+    public static String getDurationString(int seconds){
+        if (seconds >= 0){
+            int min = seconds / 60;
+            int newSec = seconds % 60;
+            int hours = min /60;
+            int newMin = min % 60;
+            System.out.println();
+    return (hours + "h" + newMin + "m" + seconds + "s");
+        }
+
+
+        return "Invalid Value";
 
     }
 
