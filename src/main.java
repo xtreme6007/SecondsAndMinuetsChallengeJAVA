5992ee301442b0e17b37828c55979b2d623fe203
xtreme6007 <prestonnichols96@gmail.com>
2021-01-05 12:05:45 -0600

    completed challenge

--- src/main.java
+++ src/main.java
@@ -1,31 +1,33 @@
 public class main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-getDurationString(193, 33);
+        getDurationString(10000000);
 
     }
-    public static String getDurationString(int minutes, int seconds){
-     if (minutes >= 0 && (seconds >= 0 && seconds <= 59)){
-         int hours = minutes / 60;
-         int newMin = minutes % 60;
-         System.out.println(hours + "h" + newMin + "m" + seconds + "s");
-         return (hours + "h" + newMin + "m" + seconds + "s");
+
+    public static String getDurationString(int minutes, int seconds) {
+        if (minutes >= 0 && (seconds >= 0 && seconds <= 59)) {
+            int hours = minutes / 60;
+            int newMin = minutes % 60;
+            System.out.println(hours + "h" + newMin + "m" + seconds + "s");
+            return (hours + "h" + newMin + "m" + seconds + "s");
 
 
         }
 
-    return "Invalid Value";
+        return "Invalid Value";
 
     }
 
-    public static String getDurationString(int seconds){
-        if (seconds >= 0){
+    public static String getDurationString(int seconds) {
+        if (seconds >= 0) {
             int min = seconds / 60;
             int newSec = seconds % 60;
-            int hours = min /60;
+            int hours = min / 60;
             int newMin = min % 60;
-            System.out.println();
-    return (hours + "h" + newMin + "m" + seconds + "s");
+
+            System.out.println(hours + "h" + newMin + "m" + newSec + "s");
+            return (hours + "h" + newMin + "m" + newSec + "s");
         }
 
 
